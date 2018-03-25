35f4c6ae9bc860e818ac82efb1be5e1f639b8414
Spares <macdonald.glen.a@gmail.com>
2018-03-25 04:45:14 +0000

    Threaded complete table grabs from BQ

--- getdata.py
+++ getdata.py
@@ -1,6 +1,7 @@
 from google.cloud import bigquery
 import pandas as pd
 import Queue
+import threading
 import logging
 
 
@@ -8,11 +9,13 @@ import logging
 projectid = "pythonproject-190701"
 dataset_id = 'cmcdataset'
 bqc = bigquery.Client(project=projectid)
+job_config = bigquery.QueryJobConfig()
+job_config.use_legacy_sql = False
 dataset_ref = bqc.dataset(dataset_id)
 
 
-def query(query,bqc):
-    query_job = bqc.query(query)
+def query(query,bqc,job_config):
+    query_job = bqc.query(query,job_config=job_config)
     return query_job
 
 def query_job_2df(query_job):
@@ -22,7 +25,63 @@ def query_job_2df(query_job):
     df = pd.DataFrame.from_records(data[:],columns = sorted(data[0]._xxx_field_to_index, key=data[0]._xxx_field_to_index.__getitem__))
     return df
 
-q = 'SELECT * FROM cmcdataset.BTC ORDER BY timestamp desc'
-query_job = query(q,bqc)
-df = query_job_2df(query_job)
+def get_table_list(bqc,job_config):
+    q = "SELECT * FROM `cmcdataset.Table_List` ORDER BY TableID"
+    query_job = query(q,bqc,job_config)
+    df = query_job_2df(query_job)
+    #does not currently support currencies that have the same symbol
+    df.drop_duplicates('symbol', inplace=True)
+    df.set_index('symbol',inplace = True)
+    return df
+
+def get_sym(sym,tablelist,bqc,job_config):
+    tableid = tablelist.loc[sym]['TableID']
+    q = "SELECT * from `cmcdataset.{}` ORDER BY timestamp".format(tableid)
+    query_job = query(q,bqc,job_config)
+    df = query_job_2df(query_job)
+    df.set_index('timestamp',inplace=True)
+    return df
+
+def get_symq(symq,qout,tablelist,bqc,job_config):
+    sym = symq.get()
+    df = get_sym(sym,tablelist,bqc,job_config)
+    qout.put(df)
+    symq.task_done()
+    return
+
+def get_sym_loop(symq,qout,tablelist,bqc,job_config):
+    while symq.qsize() > 0:
+        get_symq(symq, qout, tablelist, bqc, job_config)
+    return
+
+def get_many_syms(syms,tablelist,bqc,job_config):
+    dflist = []
+    symq = Queue.Queue()
+    qout = Queue.Queue()
+    numthreads = 5
+    threads = []
+    for sym in syms:
+        symq.put((sym))
+    for i in range(numthreads):
+        t = threading.Thread(target=get_sym_loop,args = (symq,qout,tablelist,bqc,job_config))
+        threads.append(t)
+    for i in threads:
+        i.start()
+    for i in threads:
+        i.join()
+    for i in range(qout.qsize()):
+        dflist.append(qout.get())
+    multidf = pd.concat(dflist, keys=syms)
+    return multidf
+
+
+
+
+
+
+tl = get_table_list(bqc,job_config)
+sym = 'ETH'
+df = get_sym(sym,tl,bqc,job_config)
+sym2 = 'BTC'
+df2 = get_sym(sym2,tl,bqc,job_config)
 
